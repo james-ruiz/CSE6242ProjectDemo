ca71e34ae26e1ef8c1c953d30a7ba667d87d1892
james-ruiz <james-ruiz@hotmail.com>
2023-12-03 13:34:02 -0600

    Update teamdecisions.py

--- teamdecisions.py
+++ teamdecisions.py
@@ -236,7 +236,7 @@ def app():
 
     with tab3:
         
-      play_columns               = ['play_id','posteam_fg_made_wp_delta', 'posteam_fg_missed_wp_delta', 'posteam_punt_wp_delta','posteam_pass_failed_wp_delta', 'posteam_run_failed_wp_delta', 'posteam_pass_convert_wp_delta', 'posteam_run_convert_wp_delta']
+      play_columns               = ['play_id','posteam_fg_made_wp_delta', 'posteam_fg_missed_wp_delta', 'posteam_punt_wp_delta','posteam_pass_failed_wp_delta', 'posteam_run_failed_wp_delta', 'posteam_pass_convert_wp_delta', 'posteam_run_convert_wp_delta','pass_prob','run_prob']
       column_graph               = plot_df[plot_df['play_id']==decision_play[0]]
       column_graph               = column_graph[play_columns]
       column_graph['Punt']       = column_graph['posteam_punt_wp_delta']
