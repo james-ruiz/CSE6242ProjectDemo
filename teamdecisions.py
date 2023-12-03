b7d45a532f441a17d2dabcbba43097781bdcf44d
james-ruiz <james-ruiz@hotmail.com>
2023-12-03 13:33:00 -0600

    Update teamdecisions.py

--- teamdecisions.py
+++ teamdecisions.py
@@ -241,8 +241,8 @@ def app():
       column_graph               = column_graph[play_columns]
       column_graph['Punt']       = column_graph['posteam_punt_wp_delta']
       column_graph['Field Goal'] = column_graph['posteam_fg_made_wp_delta']+column_graph['posteam_fg_missed_wp_delta']
-      column_graph['Run']        = column_graph['posteam_run_failed_wp_delta'] + column_graph['posteam_run_convert_wp_delta']
-      column_graph['Pass']       = column_graph['posteam_pass_failed_wp_delta'] + column_graph['posteam_pass_convert_wp_delta']
+      column_graph['Run']        = column_graph['posteam_run_failed_wp_delta'] + column_graph['posteam_run_convert_wp_delta']*column_graph['run_prob']
+      column_graph['Pass']       = column_graph['posteam_pass_failed_wp_delta'] + column_graph['posteam_pass_convert_wp_delta']*column_graph['pass_prob']
       column_graph               = column_graph.drop(columns=['posteam_fg_made_wp_delta', 'posteam_fg_missed_wp_delta', 'posteam_punt_wp_delta','posteam_pass_failed_wp_delta', 'posteam_run_failed_wp_delta', 'posteam_pass_convert_wp_delta', 'posteam_run_convert_wp_delta'])
       graph_df                   = column_graph.melt(id_vars='play_id', var_name='Play',value_vars=['Punt','Field Goal','Run','Pass'], value_name='Probability')
 
